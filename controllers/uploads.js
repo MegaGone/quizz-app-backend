8d8920ef04afa13f37001fdcd21896fe20212815
Javier Martínez - Junior Software Engineer <jmartinez@develsystems.com>
2022-04-17 21:51:11 -0600

    Remove trash files

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,12 +1,14 @@
 const { request, response } = require("express");
 const { validateFile } = require("../helpers");
 const { User } = require('../models')
+const fs = require('fs');
+const path = require("path");
 
 const uploadFile = async (req = request, res = response) => {
-  
+
     try {
         const pathFile = await validateFile(req.files, undefined, undefined);
-        
+
         res.status(200).send(pathFile)
     } catch (error) {
         return res.status(400).send(error)
@@ -21,14 +23,23 @@ const updateImage = async (req = request, res = response) => {
 
     model = await User.findById(id);
 
-    if(!model) {
+    if (!model) {
         return res.status(400).send('User not valid')
     }
 
-    const pathFile = await validateFile(req.files, undefined, 'users');
 
+    if (model.img) {
+
+        const pathImg = path.join(__dirname, '../uploads', model.img);
+
+        if (fs.existsSync(pathImg)) {
+            fs.unlinkSync(pathImg);
+        }
+
+    }
+
+    const pathFile = await validateFile(req.files, undefined, undefined);
     model.img = pathFile;
-
     await model.save();
 
     return res.status(200).json(model);
