957d656fbc4d05084b88752199b50beb8da60d6a
Javier Martínez <jmartinez@develsystems.com>
2022-06-10 21:29:58 -0600

    Create stats player

--- routes/stats.js
+++ routes/stats.js
@@ -10,51 +10,63 @@ const { validateFields, validatePlayer, validateJWT, verifyQuizByUser, verifyPar
 const router = Router();
 
 /*################### CREATE STAT GUEST ###################*/
+router.post('/',
+    [
+        validateJWT,
+        check('correctAnswers', 'Correct answers required').not().isEmpty(),
+        check('incorrectAnswers', 'Incorrect answers required').not().isEmpty(),
+        check('joinIn', 'Date of joinIn required').not().isEmpty(),
+        check('questions', "The quiz must be at least 5 questions").isArray({ min: 5 }),
+        check('questions.*.questionId', 'questionId not valid').isMongoId(),
+        check('questions.*.selectedIndex', 'Answer selected required').not().isEmpty(),
+        check('questions.*.time', 'Response time required').not().isEmpty(),
+        validateFields
+    ], controller.createStats)
 
 /*################### CREATE STAT GUEST ###################*/
-router.post('/guest', 
-[
-    check('quizId', "Quiz Id not valid").isMongoId(),
-    check('quizId').custom(verifyQuizById),
-    check('playerId'            , "Quiz Id not valid").not().isEmpty(),
-    validatePlayer,
-    check('playerName'          , 'Player name required').not().isEmpty(),
-    check('correctAnswers'      , 'Correct answers required').not().isEmpty(),
-    check('incorrectAnswers'    , 'Incorrect answers required').not().isEmpty(),
-    check('joinIn'              , 'Date of joinIn required').not().isEmpty(),
-    check('questions'           , "The quiz must be at least 5 questions").isArray({ min: 5}),
-    check('questions.*.questionId', 'questionId not valid').isMongoId(),
-    check('questions.*.selectedIndex' , 'Answer selected required').not().isEmpty(),
-    check('questions.*.time' ,  'Response time required').not().isEmpty(),
-    validateFields
-],controller.createStatsGuest)
+router.post('/guest',
+    [
+        check('quizId', "Quiz Id not valid").isMongoId(),
+        check('quizId').custom(verifyQuizById),
+        check('playerId', "Quiz Id not valid").not().isEmpty(),
+        validatePlayer,
+        check('playerName', 'Player name required').not().isEmpty(),
+        check('correctAnswers', 'Correct answers required').not().isEmpty(),
+        check('incorrectAnswers', 'Incorrect answers required').not().isEmpty(),
+        check('joinIn', 'Date of joinIn required').not().isEmpty(),
+        check('questions', "The quiz must be at least 5 questions").isArray({ min: 5 }),
+        check('questions.*.questionId', 'questionId not valid').isMongoId(),
+        check('questions.*.selectedIndex', 'Answer selected required').not().isEmpty(),
+        check('questions.*.time', 'Response time required').not().isEmpty(),
+        validateFields
+    ], controller.createStatsGuest)
 
 /*################### GET STATS GUEST PLAYER ###################*/
-router.get('/guest', 
-[
-    validateJWTGuest,
-    validateFields
-]
-,controller.getUserStats)
+router.get('/guest',
+    [
+        validateJWTGuest,
+        validateFields
+    ]
+    , controller.getUserStats)
 
 /*################### GET STATS BY QUIZ ###################*/
-router.get('/:id', 
-[
-    validateJWT,
-    check('id', 'Id not valid').isMongoId(),
-    verifyQuizByUser,
-    validateFields
-]
-,controller.getStatsByQuiz)
+router.get('/:id',
+    [
+        validateJWT,
+        check('id', 'Id not valid').isMongoId(),
+        verifyQuizByUser,
+        validateFields
+    ]
+    , controller.getStatsByQuiz)
 
 /*################### GET STATS BY USER ###################*/
-router.get('/:id/:user', 
-[
-    check('id', 'Quiz Id not valid').isMongoId(),
-    check('user', 'User required').not().isEmpty(),
-    verifyParticipant,
-    validateFields
-]
-, controller.getStatsByUser)
+router.get('/:id/:user',
+    [
+        check('id', 'Quiz Id not valid').isMongoId(),
+        check('user', 'User required').not().isEmpty(),
+        verifyParticipant,
+        validateFields
+    ]
+    , controller.getStatsByUser)
 
 module.exports = router;
